76af6d637dc626058b64d5848c1263bccf5dd35a
jarydh <jaryd.hodge@gmail.com>
2021-03-19 14:56:47 -0700

    fixed some bugs

--- src/my_controller/src/license_plate_image_converter.py
+++ src/my_controller/src/license_plate_image_converter.py
@@ -17,23 +17,10 @@ class imageConvert:
 		self.bridge = CvBridge()
 		self.image_sub = rp.Subscriber("/R1/pi_camera/image_raw", Image, self.new_image)
 
-<<<<<<< HEAD
-	# This method gets called whenever there is a new image on the image_raw topic
-=======
-
-
 	#Method everytime there is a new image
 
->>>>>>> license-plate-ros
 	def new_image(self, data):
 		try:
 			cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
 		except CvBridgeError as e:
 			print(e)
-
-<<<<<<< HEAD
-		# # print image for debugging
-		# cv2.imshow("1", cv_image)
-		# cv2.waitKey(3)
-=======
->>>>>>> license-plate-ros
