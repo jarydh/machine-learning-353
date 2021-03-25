8827e231cd92ccc01e13abff10cc595aae1715df
jarydh <jaryd.hodge@gmail.com>
2021-03-25 11:02:47 -0700

    changed timer to 30 seconds for time trials

--- src/my_controller/src/license_plate_reader_main.py
+++ src/my_controller/src/license_plate_reader_main.py
@@ -31,6 +31,9 @@ guess_publisher.sendStart()
 # send the stop command after 4 minutes
 stop_timer = rp.Timer(rp.Duration(4.*60), stop)
 
+# FOR TIME TRIALS: send the stop command after 30 seconds
+stop_timer = rp.Timer(rp.Duration(30), stop)
+
 rate = rp.Rate(10)
 while not rp.is_shutdown():
 	rate.sleep()
